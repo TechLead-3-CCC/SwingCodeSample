1a4305cb9c7c288783615f982b840d4a9cce7f87
TechLead-3-CCC <65514291+TechLead-3-CCC@users.noreply.github.com>
2021-07-20 08:41:50 -0400

    Add files via upload

---
+++ ESIH_SessionII_devoir1/src/Playbook.java
@@ -0,0 +1,14 @@
+import javax.swing.SwingUtilities;
+
+public class Playbook {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 SwingUtilities.invokeLater(new Runnable() {   public void run() {  
+			 new SwingDemo();  
+			 }  
+			 });  
+
+	}
+
+}
---
+++ ESIH_SessionII_devoir1/src/SwingDemo.java
@@ -0,0 +1,12 @@
+import javax.swing.*;  
+public class SwingDemo {
+	 SwingDemo() {  
+	 JFrame jfrm = new JFrame("A Simple Swing Program");  
+	 jfrm.setSize(275, 100);   
+	 JLabel jlab = new JLabel(" Swing powers the modern Java GUI.");  
+	 jfrm.getContentPane().add(jlab);  
+	 jfrm.setVisible(true);  
+	 }  
+	 
+
+}
